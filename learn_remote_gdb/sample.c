574d27fb17838a68c0bef0ce599651f4a88a1c88
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-04-25 21:28:28 +0800

    Add remote gdb example

---
+++ learn_remote_gdb/sample.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int add(int num_a, int num_b);
+
+int main()
+{
+	int num_a, num_b;
+
+	while(1) {
+		printf("Please input 2 numbers.\n");
+		scanf("%d%d",&num_a, &num_b);
+
+		printf("The result of num_a + num_b is ");
+		printf("%d\n",add(num_a, num_b));
+	}
+
+	return 0;
+}
+
+int add(int num_a,int num_b)
+{
+	return num_a + num_b;
+}
